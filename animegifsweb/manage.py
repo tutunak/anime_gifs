67bbb905bfdc1395fd800f0ad0b9c996ff957b8c
naktu <dk@tutunak.com>
2016-04-30 12:22:17 +0300

    wrong deletions

--- animegifsweb/manage.py
+++ animegifsweb/manage.py
@@ -1,6 +1,10 @@
-def add_source():
-    # TODO Function get rss link from url or return error
-
-def check_rss():
-    # TODO Function get gifs from rss and store to filesustem and write to database link
-
+#!/usr/bin/env python		
+import os		
+import sys		
+		
+if __name__ == "__main__":		
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animegifsweb.settings")		
+		
+    from django.core.management import execute_from_command_line		
+		
+    execute_from_command_line(sys.argv)
