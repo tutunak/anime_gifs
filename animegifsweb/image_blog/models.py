4726e11f944ac05882aa109c616fd94f986183d0
naktu <dk@tutunak.com>
2016-04-30 13:53:08 +0300

    add blog models

--- animegifsweb/image_blog/models.py
+++ animegifsweb/image_blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Publication(models.Model):
+    publication_date = models.DateTimeField(default=timezone.now, name='date published')
+    published = models.BooleanField(default=False)
+    publication_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.pk + " " + self.publication_name
+
+class Gif(models.Model):
+    publication_id = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    path = models.CharField(max_length=256)
+    gif_name = CharField(max_length=200)
+
+    def __str__(self):
+        return self.gif_name
