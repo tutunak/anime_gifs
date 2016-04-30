390c0d49ef28f3b8c3aa749d982f8a2181953313
naktu <dk@tutunak.com>
2016-04-30 10:53:48 +0300

    initialize two function for add source and check new gifs

---
+++ animegifsweb/manage.py
@@ -0,0 +1,6 @@
+def add_source():
+    # TODO Function get rss link from url or return error
+
+def check_rss():
+    # TODO Function get gifs from rss and store to filesustem and write to database link
+
