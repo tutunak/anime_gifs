9783cbbcb1c11c8fc2bb6508f72ff22617dcebfb
naktu <dk@tutunak.com>
2016-04-30 12:14:45 +0300

    add file conf.py for storing database configure

--- animegifsweb/animegifsweb/settings.py
+++ animegifsweb/animegifsweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from conf import DATABASES
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,7 @@ WSGI_APPLICATION = 'animegifsweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
