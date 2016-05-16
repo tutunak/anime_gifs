e8667e81da0ee3b886c3d714cac3b3538f9d3b31
naktu <dk@tutunak.com>
2016-05-16 15:23:36 +0300

    rename field

--- animegifsweb/image_blog/models.py
+++ animegifsweb/image_blog/models.py
@@ -18,7 +18,7 @@ class Gif(models.Model):
         return self.gif_name
 
 class RssUrl(models.Model):
-    rssurl = models.URLField('Rss source')
+    rss_url = models.URLField('Rss source')
     rss_name = models.CharField("Name", max_length = 200)
     active = models.BooleanField(default = 0)
 
