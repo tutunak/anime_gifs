6ce965d7efd6ce59b21627c3d4303ae636f878aa
tutunak <dk@tutunak.com>
2016-11-22 13:48:09 +0300

    add environ for working script

--- animegifsweb/scripts/rss_checker.py
+++ animegifsweb/scripts/rss_checker.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
+
 import xml.etree.ElementTree as ET
-from lxml import html
+#from lxml import html
 import os, sys
 
+project_path = ".."
+os.chdir(project_path)
+sys.path.append(project_path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animegifsweb.settings")
 
-
-if __name__ == "__main__":
-
+#os.chdir(project_path)
+import django
+django.setup()
